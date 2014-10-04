1c06af63dfb398062e0ef6266d2fde3feaa6867a
TIRUPATI HEMANTH KUMAR <tirupatihemanthkumar@gmail.com>
2014-10-04 04:52:26 +0000

    Update 2048sourcecode.py

--- 2048sourcecode.py
+++ 2048sourcecode.py
@@ -22,32 +22,32 @@ def merge(line):
     """
     Helper function that merges a single row or column in 2048
     """
-    lineList1 = list(line);
-    lineList2 = sorter(lineList1);
-    line = merger(lineList2);
+    linelist1 = list(line);
+    linelist2 = sorter(linelist1);
+    line = merger(linelist2);
     return line
 
 
-def sorter(lineList):
+def sorter(linelist):
     """ A function to separate all the non-zero and zero tiles"""
-    for x in range(len(lineList)-1):
-        for idx in range(len(lineList)):
-            if lineList[idx]==0 and idx<len(lineList)-1:
-                temp=lineList[idx];
-                lineList[idx]=lineList[idx+1];
-                lineList[idx+1]=temp;    
-    return lineList;
+    for dummy_row in range(len(linelist)-1):
+        for idx in range(len(linelist)):
+            if linelist[idx]==0 and idx<len(linelist)-1:
+                temp=linelist[idx];
+                linelist[idx]=linelist[idx+1];
+                linelist[idx+1]=temp;   
+    return linelist;
 
 
-def merger(lineList2):
+def merger(linelist2):
     """A function to merge sorted list"""
-    lineList = lineList2;
-    for idx in range(len(lineList)-1):
-        if lineList[idx]==lineList[idx+1]:
-            lineList[idx]+=lineList[idx+1];
-            lineList[idx+1]=0;
-    lineListx=sorter(lineList);
-    return lineListx;
+    linelist = linelist2;
+    for idx in range(len(linelist)-1):
+        if linelist[idx]==linelist[idx+1]:
+            linelist[idx]+=linelist[idx+1];
+            linelist[idx+1]=0;
+    linelistx=sorter(linelist);
+    return linelistx;
 
 
 class TwentyFortyEight:
@@ -60,44 +60,21 @@ class TwentyFortyEight:
         self.height = grid_height;
         self.width = grid_width;
         self.grid =[];
-        self.row=[];
-        leftList = [];
-        rightList = [];
-        upList = [];
-        downList = [];
-        for idx in range(self.width):
-           upList.append((0,idx));
-        for idx in range(self.width):
-           downList.append((self.height-1,idx));
-        for idx in range(self.height):
-           leftList.append((idx,0));
-        for idx in range(self.height):
-           rightList.append((idx,self.width-1));
-        self.library = {UP:upList,
-                   DOWN:downList,
-                   RIGHT:rightList,
-                   LEFT:leftList};
-        pass
-    
+        self.grid = [[0 for dummy_row in range(self.width)] for dummy_col in range(self.height)];
+        
     def reset(self):
         """
         Reset the game so the grid is empty.
         """
-        self.grid =[];
-        self.row = [];
-        for x in range(self.width):
-            self.row.append(0);
-        for idx in range(self.height):
-            self.grid.append(self.row);
-        pass
+        self.grid = [[0 for dummy_row in range(self.width)] for dummy_col in range(self.height)];
+       
     
     def __str__(self):
         """
         Return a string representation of the grid for debugging.
         """
-        return str(grid);
-        pass
-
+        return str(self.grid);
+    
     def get_grid_height(self):
         """
         Get the height of the board.
@@ -113,39 +90,78 @@ class TwentyFortyEight:
                             
     def move(self, direction):
         """
-        Move all tiles in the given direction and add
-        a new tile if any tiles moved.
-        """
+        Move all tiles in the given direction and add a new tile if any tiles moved."""
         
-        pass
-        
+        initial = str(self.grid);
+        if direction == UP:
+           for idx in range(self.width):
+                temp=[];
+                for xii in range(self.height):
+                    temp.append(self.grid[xii][idx]);
+                mergedlist = merge(temp);
+                for kii in range(len(mergedlist)):
+                    self.grid[kii][idx]=mergedlist[kii];
+        elif direction == DOWN:
+            for idx in range(self.width):
+                temp=[];
+                for xii in range(self.height):
+                    temp.append(self.grid[xii][idx]);
+                reverselist = temp;
+                reverselist.reverse();
+                mergedlist = merge(reverselist);
+                mergedlist.reverse();
+                for kii in range(len(mergedlist)):
+                    self.grid[kii][idx]= mergedlist[kii];
+        elif direction == LEFT:
+            for idx in range(self.height):
+                temp=[];
+                for xii in range(self.width):
+                    temp.append(self.grid[idx][xii]);
+                mergedlist = merge(temp);
+                for kii in range(len(mergedlist)):
+                    self.grid[idx][kii] = mergedlist[kii];
+        elif direction == RIGHT:
+            for idx in range(self.height):
+                temp = [];
+                for xii in range(self.width):
+                    temp.append(self.grid[idx][xii]);
+                reverselist = temp;
+                reverselist.reverse();
+                mergedlist = merge(reverselist);
+                mergedlist.reverse();
+                for kii in range(len(mergedlist)):
+                    self.grid[idx][kii] = mergedlist[kii];
+              
+        final = str(self.grid);
+        if initial != final:
+            self.new_tile();
     def new_tile(self):
         """
         Create a new tile in a randomly selected empty 
         square.  The tile should be 2 90% of the time and
         4 10% of the time.
         """
-        row = random.randint(0,self.height-1);
-        col = random.randint(0,self.width-1);
-        myList=[2,2,2,2,2,2,2,2,2,4];
-        myChoice = random.choice(myList);
-        if self.grid[row][col] == 0:
-           self.grid[row][col]=myChoice;
-        pass
+        if 0 in [self.grid[idx][xii] for idx in range(self.height) for xii in range(self.width)]:
+           while(1):
+                row = random.randint(0,self.height-1);
+                col = random.randint(0,self.width-1);
+                mylist=[2,2,2,2,2,2,2,2,2,4];
+                mychoice = random.choice(mylist);
+                if self.grid[row][col] == 0:
+                    self.grid[row][col] = mychoice;
+                    break;
         
     def set_tile(self, row, col, value):
         """
         Set the tile at position row, col to have the given value.
         """  
-        self.grid[row][col]=value;
-        pass
+        self.grid[row][col] = value;
 
     def get_tile(self, row, col):
         """
         Return the value of the tile at position row, col.
         """        
-        # replace with your code
         return self.grid[row][col];
  
     
-poc_2048_gui.run_gui(TwentyFortyEight(4, 4));
+poc_2048_gui.run_gui(TwentyFortyEight(4,4));
